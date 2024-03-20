d84d85aae2bb4bc935126fef37e6e59ac278c0a3
Antonii Belyshev <antonii.belyshev@gmail.com>
2024-03-20 09:14:49 +0000

    small fixes

--- utils/bmds_mcmc.py
+++ utils/bmds_mcmc.py
@@ -1,5 +1,6 @@
 from typing import Union, Callable
 from tqdm import trange
+from itertools import combinations
 
 
 import torch
@@ -19,70 +20,86 @@ class BMDSMCMC:
             self,
             *,
             m: int = 10,
+            init_lr: float = 1e-2,
             gamma: float = -0.8,
             sample_steps: int = 1000,
             sample_size: int = 100,
             n_iter: int = 10000,
             init_log_diff: float = 4.0,
-            n_neighbors: Union[int, float] = 0.2,
     ):
         self.m = m
+        self.init_lr = init_lr
         self.gamma = gamma
         self.sample_steps = sample_steps
         self.sample_size = sample_size
         self.n_iter = n_iter
         self.init_log_diff = init_log_diff
-        self.n_neighbors = n_neighbors
 
-    @staticmethod
-    def create_grad_log_likelihood(
-            scale: torch.Tensor,
-            dist_sqr: torch.Tensor,
-            neighbors_idx: torch.Tensor,
+    def create_log_likelihood(
+        self,
+        dist_mat: torch.Tensor,
+        scale: torch.Tensor,
     ) -> Callable[[torch.Tensor], torch.Tensor]:
-        def grad_log_likelihood(x):
-            neighbors = x[neighbors_idx]
-            diffs = x - neighbors
-            d_sqr = diffs.pow(2).sum(dim=2, keepdims=True)
-            return ((dist_sqr - d_sqr) / d_sqr.pow(2) * diffs).mean(dim=0) - x / scale.pow(2)
-        return grad_log_likelihood
+        idx1, idx2 = torch.tensor([*combinations(range(len(dist_mat)), 2)]).T
+        dist_sqr = dist_mat[idx1, idx2].pow(2)
+
+        def log_likelihood(x: torch.Tensor) -> torch.Tensor:
+            d_sqr = (x[idx1] - x[idx2]).pow(2).sum(dim=1)
+            return -torch.log(d_sqr).sum() - (dist_sqr / d_sqr).sum() - (x ** 2 / scale ** 2 / 2).sum() - torch.log(scale).sum() * len(x)
+        
+        return log_likelihood
+
+    def create_grad_log_likelihood(
+        self,
+        dist_mat: torch.Tensor,
+        scale: torch.Tensor,
+    ) -> Callable[[torch.Tensor], torch.Tensor]:
+        def grad_log_likelihood(x: torch.Tensor) -> torch.Tensor:
+            grad = -X / scale ** 2
+            for idx1, idx2 in combinations(range(len(dist_mat)), 2):
+                diff = x[idx1] - x[idx2]
+                d_sqr = diff.pow(2).sum()
+                component = 2 * (dist_mat[idx1, idx2].pow(2) - d_sqr) / d_sqr.pow(2) * diff
+                grad[idx1] -= component
+                grad[idx2] += component
+            return grad
 
     def create_epsilon_scheduler(
             self,
             epoch: int,
     ) -> Callable[[int], float]:
-        lr = self.get_lr(epoch)
+        lr = self.init_lr * self.get_lr(epoch)
         return lambda _: lr
 
     def get_lr(
             self,
             epoch: int,
     ):
-        return (epoch + 1) ** self.gamma
+        return self.init_lr * (epoch + 1) ** self.gamma
 
     def fit(
             self,
             dist_mat_train: torch.Tensor,
     ):
-        scale = torch.linspace(0, self.init_log_diff, self.m, device=DEVICE)
+        scale = torch.exp(torch.linspace(0, self.init_log_diff, self.m, device=DEVICE))
         scale = scale / scale.pow(2).sum().pow(0.5)
-        samples = torch.randn(len(dist_mat_train), self.sample_size, self.m, device=DEVICE) * scale
+        samples = torch.randn(self.sample_size, len(dist_mat_train), self.m, device=DEVICE) * scale
 
         dist_mat_train = (dist_mat_train / dist_mat_train.max()).to(DEVICE)
-        if isinstance(self.n_neighbors, float):
-            self.n_neighbors = int(self.n_neighbors * len(samples))
-        neighbors_idx = dist_mat_train.argsort(dim=0)[1:self.n_neighbors + 1]
 
-        dist_sqr = dist_mat_train[torch.arange(len(samples))][neighbors_idx].pow(2).unsqueeze(2)
-
+        vals = []
         for epoch in trange(self.n_iter):
             x = samples[-1]
-            scale = samples[-self.sample_size:].pow(2).mean(dim=(0, 1))
-            grad_log_likelihood = self.create_grad_log_likelihood(scale, dist_sqr, neighbors_idx)
+            scale = samples[-self.sample_size:].pow(2).mean(dim=(0, 1)).pow(0.5)
+            log_likelihood = self.create_log_likelihood(dist_mat_train, scale)
             epsilon_scheduler = self.create_epsilon_scheduler(epoch)
-            samples = langevin_dynamics(x, self.sample_steps, grad_log_likelihood, epsilon_scheduler)
+            samples, vals_ = langevin_dynamics(x, self.sample_steps, log_likelihood, epsilon_scheduler)
+            if torch.isnan(samples).any():
+                aaa
+            vals.extend(vals_)
 
         self.samples = samples
+        self.vals = vals
 
     def fit_transform(
             self,
--- utils/mcmc.py
+++ utils/mcmc.py
@@ -1,22 +1,28 @@
-from typing import Callable, List
+from typing import Callable, List, Optional
 
 import torch
+from typing import Tuple, List
 
 
 def langevin_dynamics(
         x_init: torch.Tensor,
         n_iter: int,
-        compute_grad: Callable[[torch.Tensor], torch.Tensor],
+        log_likelihood: Callable[[torch.Tensor], torch.Tensor],
         epsilon_scheduler: Callable[[int], float],
-) -> torch.Tensor:
+) -> Tuple[torch.Tensor, List[torch.Tensor]]:
+    vals = []
     samples = [x_init]
     x = x_init
     for k in range(n_iter):
-        grad = compute_grad(x)
+        x.requires_grad = True
+        val = log_likelihood(x)
+        grad = torch.autograd.grad(val, x)[0]
+        x.requires_grad = False
+        vals.append(val.detach())
         epsilon = epsilon_scheduler(k)
         x = langevin_step(x, grad, epsilon)
         samples.append(x)
-    return torch.cat(samples, dim=0)
+    return torch.stack(samples, dim=0), vals
 
 
 def langevin_step(
